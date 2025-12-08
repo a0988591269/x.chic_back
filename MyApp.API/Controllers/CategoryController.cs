829697b78cc411b629812f2f4f509b5276990c47
Benson <a0988591269@gmail.com>
2025-12-08 23:05:14 +0800

    .

--- MyApp.API/Controllers/CategoryController.cs
+++ MyApp.API/Controllers/CategoryController.cs
@@ -1,8 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using MyApp.Application.Features.Categories.Queries.GetCategory;
-using MyApp.Application.Services.Categories.Queries;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace MyApp.API.Controllers
 {
