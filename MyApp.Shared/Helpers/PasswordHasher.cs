b64def360008511cae2b6fefd3c273dbf3f9190b
MSI\a0988 <a0988591269@gmail.com>
2025-12-24 09:09:26 +0800

    20251224 - fix csproj

---
+++ MyApp.Shared/Helpers/PasswordHasher.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyApp.Shared.Helpers
+{
+    public static class PasswordHasher
+    {
+        private static readonly PasswordHasher<object> _hasher = new();
+
+        // 註冊 / 修改密碼用
+        public static string Hash(string password)
+        {
+            return _hasher.HashPassword(null!, password);
+        }
+
+        // 登入驗證用
+        public static bool Verify(string password, string hashedPassword)
+        {
+            var result = _hasher.VerifyHashedPassword(
+                null!,
+                hashedPassword,
+                password
+            );
+
+            return result == PasswordVerificationResult.Success
+                || result == PasswordVerificationResult.SuccessRehashNeeded;
+        }
+    }
+}
