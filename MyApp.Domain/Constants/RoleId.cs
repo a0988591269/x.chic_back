a82a9659e1d4c50fedb60b65b785e841e3ee6257
MSI\a0988 <a0988591269@gmail.com>
2026-01-09 18:42:03 +0800

    20260109 - add AddRole

---
+++ MyApp.Domain/Constants/RoleId.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Domain.Constants
+{
+    public static class RoleId
+    {
+        /// <summary>
+        /// 管理員
+        /// </summary>
+        public const int Admin = 1;
+
+        /// <summary>
+        /// 一般會員
+        /// </summary>
+        public const int Member = 2;
+    }
+}
--- MyApp.Domain/Entities/User.cs
+++ MyApp.Domain/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyApp.Domain.Constants;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,33 +12,6 @@ namespace MyApp.Domain.Entities
     [Table("Users")]
     public class User : BaseEntity
     {
-        /// <summary>
-        /// 加入 Private Constructor
-        /// 這是給 EF Core 用的 (EF 需要用它來從資料庫還原物件)
-        /// 同時也阻止了外部使用 'new User()'
-        /// </summary>
-        private User() { }
-
-        /// <summary>
-        /// 靜態工廠方法 (唯一的公開建立入口)
-        /// </summary>
-        public static User Create(string email, string hashedPassword, string? name)
-        {
-            // 在這裡可以放入「防衛條款 (Guard Clauses)」
-            // 確保資料不合法時，根本無法建立物件
-            if (string.IsNullOrWhiteSpace(email)) throw new ArgumentException("Email is required");
-
-            return new User
-            {
-                Email = email,
-                HashedPassword = hashedPassword,
-                Name = name,
-                UserUuid = Guid.NewGuid(), // 可以在這裡明確賦值
-                Tier = 0,                  // 預設值明確化
-                Status = 1                 // 預設值明確化
-            };
-        }
-
         /// <summary>
         /// 主鍵 (PK)
         /// </summary>
@@ -80,5 +54,52 @@ namespace MyApp.Domain.Entities
 
         // nav
         public ICollection<UserRole>? UserRoles { get; set; }
+
+        /// <summary>
+        /// 加入 Private Constructor
+        /// 這是給 EF Core 用的 (EF 需要用它來從資料庫還原物件)
+        /// 同時也阻止了外部使用 'new User()'
+        /// </summary>
+        private User() { }
+
+        /// <summary>
+        /// 靜態工廠方法 (唯一的公開建立入口)
+        /// </summary>
+        public static User Create(string email, string hashedPassword, string? name)
+        {
+            var user = new User
+            {
+                Email = email,
+                HashedPassword = hashedPassword,
+                Name = name,
+                UserUuid = Guid.NewGuid(), // 可以在這裡明確賦值
+                Tier = 0,                  // 預設值明確化
+                Status = 1,                // 預設值明確化
+                UserRoles = new List<UserRole>()
+            };
+
+            user.AddRole(RoleId.Member);
+
+            return user;
+        }
+
+        /// <summary>
+        /// 未來如果有 "升級 VIP" 的邏輯，也可以重複使用
+        /// </summary>
+        /// <param name="roleId"></param>
+        public void AddRole(int roleId)
+        {
+            UserRoles ??= new List<UserRole>(); // 防呆：如果原本是 null 就 new 一個
+
+            // 避免重複加入
+            if (!UserRoles.Any(ur => ur.RoleId == roleId))
+            {
+                UserRoles.Add(new UserRole
+                {
+                    // UserId 暫時不用設，EF Core 儲存時會自動關聯到這個 User
+                    RoleId = roleId
+                });
+            }
+        }
     }
 }
